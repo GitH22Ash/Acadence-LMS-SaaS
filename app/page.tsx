73d69cdd9b60a793918bb52b74b1acc833093560
Ayush <ayushbijalwan.ab@gmail.com>
2025-09-26 22:43:17 +0530

    Setup: Next.js and Shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { Button } from '@/components/ui/button';
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+      <h1 className='text-2xl underline'>Welcome to the Home Page</h1>
+      <Button>Click Me</Button>
+    </div>
   )
 }
 
